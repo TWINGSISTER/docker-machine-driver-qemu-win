4a9ae388ee2e0d9bb385def96619dcc83bdac4b7
Twingsister <twingsister@gmail.com>
2020-06-21 00:44:33 +0200

    Changed to GoClipse structure

--- src/windriver/cmd/main.go
+++ src/windriver/cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/docker/machine/libmachine/drivers/plugin"
-	"github.com/intel-iot-devkit/docker-machine-driver-qemu"
+	"github.com/TWINGSISTER/docker-machine-driver-qemu-win/src/windriver"
 )
 
 func main() {
